6e962a8940cf9a91d91756a18222c989c954ac90
A.Sacharov <a.sacharov@gmx.de>
2023-10-29 17:26:00 +0100

    improve project; improve style check; add clang  files;

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@
 //-----------------------------------------------------------------------------
 // includes
 //-----------------------------------------------------------------------------
+#include <getopt.h>
 #include <chrono>
 #include <condition_variable>
 #include <filesystem>
 #include <fstream>
 #include <fswatch.hpp>
-#include <getopt.h>
 #include <iostream>
 #include <mutex>
 #include <string>
@@ -33,9 +33,9 @@ using namespace std::chrono_literals;
  * @brief event plan structure
  */
 struct TaskEvent {
-public:
-    std::mutex event_mutex;
-    std::condition_variable event_condition;
+ public:
+  std::mutex event_mutex;
+  std::condition_variable event_condition;
 };
 
 //-----------------------------------------------------------------------------
@@ -60,21 +60,21 @@ TaskEvent taskEventStopTest;
 * @brief   show version for winconfig.
 * @param prog - name of program
 ****************************************************************************/
-static void ShowVersion(const char *prog) {
-    auto verString{"1.0"};
-    // info
-    std::cout << prog << " v." << verString << std::endl;
+static void ShowVersion(const char* prog) {
+  auto verString{"1.0"};
+  // info
+  std::cout << prog << " v." << verString << std::endl;
 }
 
 /************************************************************************/ /**
-* @fn      void DisplayHelp(const char* prog)
+* @fn      void ShowUsage(const char* prog)
 * @brief   view help
 * @param  prog - Name of the program in the display help
 ****************************************************************************/
-static void DisplayHelp(const char *prog) {
-    std::cout << "Usage: " << prog << " [OPTION]\n"
-              << "  -v, --version            version\n"
-              << "  -h, --help               this message\n\n";
+static void ShowUsage(const char* prog) {
+  std::cout << "Usage: " << prog << " [OPTION]\n"
+            << "  -v, --version            version\n"
+            << "  -h, --help               this message\n\n";
 }
 
 /************************************************************************/ /**
@@ -82,41 +82,40 @@ static void DisplayHelp(const char *prog) {
 * @param argc - number parameters in command line
 * @param argv - command line parameters as array
 ****************************************************************************/
+static void ProgramOptions(int argc, char* argv[]) {
+  for (;;) {
+    int option_index = 0;
+    static const char* short_options = "h?v";
+    static const struct option long_options[] = {
+       {"help", no_argument, 0, 0},
+       {"version", no_argument, 0, 'v'},
+       {0, 0, 0, 0},
+    };
 
-static void ProcessOptions(int argc, char *argv[]) {
-    for (;;) {
-        int option_index = 0;
-        static const char *short_options = "h?v";
-        static const struct option long_options[] = {
-                {"help", no_argument, 0, 0},
-                {"version", no_argument, 0, 'v'},
-                {0, 0, 0, 0},
-        };
+    int var = getopt_long(argc, argv, short_options, long_options, &option_index);
 
-        int var = getopt_long(argc, argv, short_options, long_options, &option_index);
-
-        if (var == EOF) {
-            break;
-        }
-        switch (var) {
-            case 0:
-                DisplayHelp(argv[0]);
-                break;
-            case '?':
-            case 'h': {
-                DisplayHelp(argv[0]);
-                exit(EXIT_SUCCESS);
-            }
-            case 'v': {
-                ShowVersion(argv[0]);
-                exit(EXIT_SUCCESS);
-            }
-            default: {
-                DisplayHelp(argv[0]);
-                exit(-1);
-            }
-        }
+    if (var == EOF) {
+      break;
     }
+    switch (var) {
+      case 0:
+        ShowUsage(argv[0]);
+        break;
+      case '?':
+      case 'h': {
+        ShowUsage(argv[0]);
+        exit(EXIT_SUCCESS);
+      }
+      case 'v': {
+        ShowVersion(argv[0]);
+        exit(EXIT_SUCCESS);
+      }
+      default: {
+        ShowUsage(argv[0]);
+        exit(-1);
+      }
+    }
+  }
 }
 
 /**
@@ -126,22 +125,22 @@ static void ProcessOptions(int argc, char *argv[]) {
  *          otherwise - false
  **/
 static bool HandleGetChar() {
-    char var = getchar();
-    switch (var) {
-        case '\n':
-            break;
-        case 'q': {
-            std::cout << "Received QUIT command\nExiting.." << std::endl;
-            return (true);
-        }
-        default: {
-            std::cout << "Console \n"
-                      << " Key options are:\n"
-                      << "  q - quit from the program" << std::endl;
-            break;
-        }
+  char var = getchar();
+  switch (var) {
+    case '\n':
+      break;
+    case 'q': {
+      std::cout << "Received QUIT command\nExiting.." << std::endl;
+      return (true);
     }
-    return false;
+    default: {
+      std::cout << "Console \n"
+                << " Key options are:\n"
+                << "  q - quit from the program" << std::endl;
+      break;
+    }
+  }
+  return false;
 }
 
 /**
@@ -150,14 +149,14 @@ static bool HandleGetChar() {
  * @param all_tasks_wakeup - wakeup all tasks but not only file system event driving tasks
  */
 void WakeUpTasks(bool all_tasks_wakeup = true) {
-    if (all_tasks_wakeup) {
-        std::unique_lock lck(taskEventStopFswatcher.event_mutex);
-        taskEventStopFswatcher.event_condition.notify_all();// Wakes up stop a file system watcher
-    }
-    {
-        std::unique_lock lck(taskEventStopTest.event_mutex);
-        taskEventStopTest.event_condition.notify_all();// Wakes up a displacement task
-    }
+  if (all_tasks_wakeup) {
+    std::unique_lock lck(taskEventStopFswatcher.event_mutex);
+    taskEventStopFswatcher.event_condition.notify_all();   // Wakes up stop a file system watcher
+  }
+  {
+    std::unique_lock lck(taskEventStopTest.event_mutex);
+    taskEventStopTest.event_condition.notify_all();   // Wakes up a task
+  }
 }
 
 /**
@@ -168,95 +167,90 @@ void WakeUpTasks(bool all_tasks_wakeup = true) {
  * @param token - stop task token
  */
 void TaskWorkerFsWatcher(std::stop_token token) {
-    using namespace std::chrono_literals;
-    const auto waitDuration = 2s;
-    auto watcher = fswatch("/tmp");
+  using namespace std::chrono_literals;
+  const auto waitDuration = 2s;
+  auto watcher = fswatch("/tmp");
 
-    // add watching events
-    watcher.on({fswatch::Event::FILE_CLOSED, fswatch::Event::FILE_MODIFIED, fswatch::Event::FILE_DELETED},
-               [&]([[maybe_unused]] auto &event) {
-                   WakeUpTasks(false);// Wake up sleeping tasks by an event in the file system
-               });
+  // add watching events
+  watcher.on({fswatch::Event::FILE_CLOSED, fswatch::Event::FILE_MODIFIED, fswatch::Event::FILE_DELETED},
+             [&]([[maybe_unused]] auto& event) {
+               WakeUpTasks(false);   // Wake up sleeping tasks by an event in the file system
+             });
 
-    // Register a stop callback for stop task
-    std::stop_callback stop_cb(token, [&]() {
-        // Wake up thread on stop request
-        taskEventStopFswatcher.event_condition.notify_all();
-    });
+  // Register a stop callback for stop task
+  std::stop_callback stop_cb(token, [&]() {
+    // Wake up thread on stop request
+    taskEventStopFswatcher.event_condition.notify_all();
+  });
 
-    // start thread with stop fs-watcher task
-    std::thread stop_watching_task([&]() {
-        // create observer
-        while (true) {
-            // Start of locked block
-            std::unique_lock lck(taskEventStopFswatcher.event_mutex);
-            taskEventStopFswatcher.event_condition.wait(lck, [&, token]() {
-                return token.stop_requested();
-            });
+  // start thread with stop fs-watcher task
+  std::thread stop_watching_task([&]() {
+    // create observer
+    while (true) {
+      // Start of locked block
+      {
+        std::unique_lock lck(taskEventStopFswatcher.event_mutex);
+        taskEventStopFswatcher.event_condition.wait(lck, [&, token]() { return token.stop_requested(); });
+      }
 
-            //Stop if requested to stop
-            if (token.stop_requested()) {
-                std::printf("Stop requested for a stop watcher task\n");
-                break;
-            }
-        }// End of while loop
-        watcher.stop();
-        // wakeup watcher via event in file system
-        std::ofstream tmpfile;
-        tmpfile.open("/tmp/~wakeup", std::ios_base::trunc);
-        tmpfile << "Wakeup\n";
-        tmpfile.close();
-        std::printf("Stop filesystem watcher task stopped\n");
-    });
+      //Stop if requested to stop
+      if (token.stop_requested()) {
+        std::printf("Stop requested for a stop watcher task\n");
+        break;
+      }
+    }   // End of while loop
+    watcher.stop();
+    // wakeup watcher via event in file system
+    std::ofstream tmpfile;
+    tmpfile.open("/tmp/~wakeup", std::ios_base::trunc);
+    tmpfile << "Wakeup\n";
+    tmpfile.close();
+    std::printf("Stop filesystem watcher task stopped\n");
+  });
 
-    try {
-        watcher.start();
-    } catch (std::filesystem::filesystem_error &error) {
-        std::printf("Filesystem exception was caught: %s\n", error.what());
-    } catch (std::exception &error) {
-        std::printf("Exception was caught: %s\n", error.what());
-    } catch (...) {
-        std::printf("Unknown exception was caught\n");
-    }
+  try {
+    watcher.start();
+  } catch (std::filesystem::filesystem_error& error) {
+    std::printf("Filesystem exception was caught: %s\n", error.what());
+  } catch (std::exception& error) {
+    std::printf("Exception was caught: %s\n", error.what());
+  } catch (...) {
+    std::printf("Unknown exception was caught\n");
+  }
 
-    stop_watching_task.join();
+  stop_watching_task.join();
 
-    std::printf("Filesystem watcher task stopped\n");
+  std::printf("Filesystem watcher task stopped\n");
 }
 
 /**
- * @brief Displacement connection main function
+ * @brief main function
  * @param lptr - log appender
  * @param cfg_converter - configuration
  * @param token - stop task token
  */
 void TaskWorker_Test(std::stop_token token) {
-    using namespace std::chrono_literals;
-    const auto waitDurationDef = 3000ms;
+  using namespace std::chrono_literals;
 
-    // Register a stop callback
-    std::stop_callback stop_cb(token, [&]() {
-        // Wake thread on stop request
-        taskEventStopTest.event_condition.notify_all();
-    });
+  // Register a stop callback
+  std::stop_callback stop_cb(token, [&]() {
+    // Wake thread on stop request
+    taskEventStopTest.event_condition.notify_all();
+  });
 
-    // create displacement context
-    auto sooner = waitDurationDef;
+  while (true) {
+    {
+      std::unique_lock lck(taskEventStopTest.event_mutex);
+      taskEventStopTest.event_condition.wait_for(lck, 1s);
+    }
+    //Stop if requested to stop
+    if (token.stop_requested()) {
+      std::printf("Stop requested for a task\n");
+      break;
+    }
+  }   // End of while loop
 
-    while (true) {
-        // observe serves states
-        // Start of locked block
-        std::unique_lock lck(taskEventStopTest.event_mutex);
-        taskEventStopTest.event_condition.wait_for(lck, std::chrono::milliseconds(sooner));
-
-        //Stop if requested to stop
-        if (token.stop_requested()) {
-            std::printf("Stop requested for a displacement connection task\n");
-            break;
-        }
-    }// End of while loop
-
-    std::printf("Displacement connection task stopped.\n");
+  std::printf("Test task stopped.\n");
 }
 
 /************************************************************************/ /**
@@ -267,44 +261,44 @@ void TaskWorker_Test(std::stop_token token) {
 * @return EXIT_SUCCESS if successfully, otherwise - EXIT_FAILURE
 ****************************************************************************/
 
-int main(int argc, char **argv) {
-    std::stop_source stopSource;// Create a stop source
-    std::thread workerFswatcher;
-    std::thread workerTest;
+int main(int argc, char** argv) {
+  std::stop_source stopSource;   // Create a stop source
+  std::thread workerFswatcher;
+  std::thread workerTest;
 
-    //----------------------------------------------------------
-    // parse parameters
-    //----------------------------------------------------------
-    ProcessOptions(argc, argv);
+  //----------------------------------------------------------
+  // parse parameters
+  //----------------------------------------------------------
+  ProgramOptions(argc, argv);
 
-    // show information
-    ShowVersion(argv[0]);
+  // show information
+  ShowVersion(argv[0]);
 
-    //----------------------------------------------------------
-    // go to idle in main
-    //----------------------------------------------------------
-    // Create all workers and pass stop tokens
-    workerTest = std::move(std::thread(TaskWorker_Test, stopSource.get_token()));
-    // start task filesystem watcher
-    workerFswatcher = std::move(std::thread(TaskWorkerFsWatcher, stopSource.get_token()));
+  //----------------------------------------------------------
+  // go to idle in main
+  //----------------------------------------------------------
+  // Create all workers and pass stop tokens
+  workerTest = std::move(std::thread(TaskWorker_Test, stopSource.get_token()));
+  // start task filesystem watcher
+  workerFswatcher = std::move(std::thread(TaskWorkerFsWatcher, stopSource.get_token()));
 
-    // main loop or sleep
-    while (true) {
-        if (HandleGetChar()) {
-            break;
-        }
+  // main loop or sleep
+  while (true) {
+    if (HandleGetChar()) {
+      break;
     }
+  }
 
-    std::printf("Request stop all tasks\n");
-    // set token to stop all worker
-    stopSource.request_stop();
+  std::printf("Request stop all tasks\n");
+  // set token to stop all worker
+  stopSource.request_stop();
 
-    // wakeup all tasks
-    WakeUpTasks(true);
+  // wakeup all tasks
+  WakeUpTasks(true);
 
-    // Join threads
-    workerTest.join();
-    workerFswatcher.join();
+  // Join threads
+  workerTest.join();
+  workerFswatcher.join();
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
